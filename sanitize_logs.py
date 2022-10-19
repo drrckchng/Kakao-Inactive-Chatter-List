645fd8d69502998d6a47d2d4c06db5d2ffc25550
drrckchng <19944575+drrckchng@users.noreply.github.com>
2022-10-19 15:07:05 +0900

    Rename file and wrap in function

---
+++ sanitize_logs.py
@@ -0,0 +1,23 @@
+import os
+import re
+
+def updateLog():
+    # Open consolidated log file
+    final_log_txt= open('Talk_logs.txt', 'w')
+
+    # Loop through all log files in logs/ directory
+    for filename in sorted(os.listdir('logs/'), key=str.lower):
+        # Read log file (testing)
+        log_txt = open('logs/' + filename, 'r')
+        log = log_txt.readlines()
+
+        # Delete first 5 lines of each new log file
+        del log[0:5]
+
+        # Regex pattern for valid chat message
+        pattern = re.compile('[A-Z][a-z]{2} [0-9]{1,2}, [0-9]{4} [0-9]{1,2}:[0-9]{2} [A-Z]{2},.*')
+
+        # Loop through log file and write to final txt output
+        for x in log:
+            if pattern.match(x):
+                final_log_txt.write(x)
