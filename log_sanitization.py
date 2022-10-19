9bcf6e604b4f683c4bd5158245401ef69d7cf74e
drrckchng <19944575+drrckchng@users.noreply.github.com>
2022-10-19 15:00:54 +0900

    Loop through all logs in logs folder and insert them into final logs txt file

--- log_sanitization.py
+++ log_sanitization.py
@@ -1,20 +1,24 @@
+import os
 import re
 
 # Open consolidated log file
 final_log_txt= open('Talk_logs.txt', 'w')
 
-# Read log file (testing)
-log_txt = open('logs/Talk_2022.10.19 02:18-1.txt', 'r')
-log = log_txt.readlines()
 
-# Delete first 5 lines of each new log file
-del log[0:5]
+for filename in sorted(os.listdir('logs/'), key=str.lower):
+    # Read log file (testing)
+    log_txt = open('logs/' + filename, 'r')
+    log = log_txt.readlines()
 
-# Regex pattern for valid chat message
-pattern = re.compile('[A-Z][a-z]{2} [0-9]{1,2}, [0-9]{4} [0-9]{1,2}:[0-9]{2} [A-Z]{2},.*')
+    # Delete first 5 lines of each new log file
+    del log[0:5]
 
-# Loop through log file
-for x in log:
-    if pattern.match(x):
-        final_log_txt.write(x)
-    
+    # Regex pattern for valid chat message
+    pattern = re.compile('[A-Z][a-z]{2} [0-9]{1,2}, [0-9]{4} [0-9]{1,2}:[0-9]{2} [A-Z]{2},.*')
+
+    # Loop through log file
+    for x in log:
+        if pattern.match(x):
+            final_log_txt.write(x)
+    print('inserted' + filename)
+        
