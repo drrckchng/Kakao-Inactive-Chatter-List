7ae103f5cb189c72c16ee96016e223035c48cfd7
drrckchng <19944575+drrckchng@users.noreply.github.com>
2022-10-19 19:09:27 +0900

    Get inactive chatters

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
+from datetime import timedelta
 import re
 import pandas as pd
 import sanitize_logs
 
 # Ask user input to update logs
 while True:
-    update = input('Update logs? [y/n]')
+    update = input('Update logs? [y/n] ')
     if update.lower() in ['y', 'yes']:
         sanitize_logs.updateLog()
+        break
     elif update.lower() in ['n', 'no']:
         break
     else:
@@ -15,15 +17,20 @@ while True:
 # Regex Patterns
 date_pattern = re.compile('[A-Z][a-z]{2} [0-9]{1,2}, [0-9]{4} [0-9]{1,2}:[0-9]{2} [A-Z]{2}')
 
-# Empty array
+# Empty list
 lines = []
 
 # Loop through each line in final logs
 with open('Talk_logs.txt', 'r') as f:
     for line in f:
-        # Split user and message
-        user = line.split('M, ')[1].split(' : ')[0]
-        message = line.split('M, ')[1].split(' : ')[1]
+        if 'joined this chat room.' in line:
+            # Split user and message
+            user = line.split('M, ')[1].split(' joined')[0]
+            message = line.split('M, ')[1].split(' joined')[1]
+        else:
+            # Split user and message
+            user = line.split('M, ')[1].split(' : ')[0]
+            message = line.split('M, ')[1].split(' : ')[1]
         # Use regex to get date and convert to pd datetime obj
         date = pd.to_datetime(date_pattern.search(line).group())
 
@@ -33,3 +40,20 @@ with open('Talk_logs.txt', 'r') as f:
             'user': user,
             'message': message
         })
+
+# user_list = set()
+# for line in lines:
+#     user_list.add(line['user'])
+
+df = pd.DataFrame(lines)
+
+# Dates
+end_date = lines[-1]['date']
+start_date = end_date - timedelta(days=30)
+
+users = pd.unique(df['user'].to_list())
+
+active_users = pd.unique(df[df['date'].between(start_date, end_date)]['user'].to_list())
+
+inactive_users = list(set(users) - set(active_users))
+print(inactive_users)
